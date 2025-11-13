19ef97db348e073612427d2676024a00d9ee29e9
omarakamal <omaraakamal@gmail.com>
2025-11-13 13:42:25 +0300

    update

--- class-code/js/app.js
+++ class-code/js/app.js
@@ -2,11 +2,22 @@ const submitButtonElement = document.querySelector('#submit-button')
 
 const todoInputElement = document.querySelector('#todo-input')
 
-console.log(todoInputElement)
+const todoListElement = document.querySelector('#todo-list')
 
 function addTwoDoItem(){
+    console.log(todoInputElement.value)
     console.log('to do item added')
+    const newTodoElement = document.createElement('li')
+    newTodoElement.textContent = todoInputElement.value
+    console.log(newTodoElement)
+
+
 }
+
+// 1. create a new li element
+// 2. put the text inside of the element that is what the user typed
+// 3. add the element to the page
 
 // addEventListener('eventType', function that runs)
 submitButtonElement.addEventListener('click',addTwoDoItem)
+
